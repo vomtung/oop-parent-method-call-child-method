0934e4d2cd8749acd5aebc438a283c74913b2506
Tung Vo <vominhtung@tma.com.vn>
2020-01-14 15:27:29 +0700

    init project

---
+++ oop-example1/src/com/company/Child.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Child extends Parent {
+
+    @Override
+    public void sing2(){
+        System.out.println("Child sing");
+    }
+
+}
---
+++ oop-example1/src/com/company/Parent.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Parent {
+
+
+    public void sing(){
+        sing2();
+    }
+
+    public void sing2(){
+        System.out.println("parent");
+    }
+}

7cc8322e1e0b0803f366f39ee5fe141eb9ae0e4c
Tung Vo <vominhtung@tma.com.vn>
2020-04-21 11:57:20 +0700

    example decimal format

--- oop-example1/src/com/company/Main.java
+++ oop-example1/src/com/company/Main.java
@@ -1,11 +1,20 @@
 package com.company;
 
+import java.text.DecimalFormat;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         Parent m = new Child();
         m.sing();
+
+
+        double[] numbers = {1.16763, 33.0, 44444.1111, 3, 517827.17, 44444.101, 4444444};
+        DecimalFormat decimalFormat = new DecimalFormat("#.00"); decimalFormat.setGroupingUsed(true);
+        decimalFormat.setGroupingSize(3);
+        System.out.println("adding commas to number in Java using DecimalFormat class");
+        for (double d : numbers) { System.out.println(decimalFormat.format(d)); }
     }
 
 
